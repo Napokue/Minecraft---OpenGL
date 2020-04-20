c83bc36d6e9b8b5d16c70959a983624ae4186f47
Matijs <matijstoonen@gmail.com>
2020-04-20 18:33:23 +0200

    now uses vertex color in fragment shader

--- Minecraft/Main.cpp
+++ Minecraft/Main.cpp
@@ -11,17 +11,20 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 const char* vertexShaderSource = "#version 460 core\n"
-"layout (location = 0) in vec3 aPos;\n // the position variable has attribute position 0"
+"layout (location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   vertexColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 460 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vertexColor;\n"
 "}\n\0";
 
 const char* yellowFragmentShaderSource = "#version 460 core\n"
